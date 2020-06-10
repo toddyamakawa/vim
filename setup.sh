51bddace0cf117c7bea367cb00f8fbed8d8d20f7
Todd Yamakawa <todd.yamakawa@gmail.com>
2020-06-09 20:58:58 -0500

    setup.sh Adding $XDG_CONFIG_HOME.

--- setup.sh
+++ setup.sh
@@ -1,14 +1,23 @@
 #!/usr/bin/env bash
-declare -r script=$(readlink -f $BASH_SOURCE)
-declare -r script_dir=$(dirname $script)
+declare -r CURRENT_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
+
+XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-$HOME/.config}
+[[ ! -d $XDG_CONFIG_HOME ]] && mkdir -pv $XDG_CONFIG_HOME
+
+if [[ ! -d "$XDG_CONFIG_HOME/vim" ]]; then
+	ln -s $CURRENT_DIR "$XDG_CONFIG_HOME/vim"
+
+#elif [[ ! -L "$XDG_CONFIG_HOME/vim" ]]; then
+	# TODO: Check if symlink is correct
+fi
 
 # Download vim-plug
-plug=$script_dir/autoload/plug.vim
-if [[ ! -f $plug ]]; then
+plug="$CURRENT_DIR/autoload/plug.vim"
+if [[ ! -f "$plug" ]]; then
 	echo "Downloading plug.vim..."
 	curl \
 		-fL \
-		-o $plug \
+		-o "$plug" \
 		--create-dirs \
 		https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
